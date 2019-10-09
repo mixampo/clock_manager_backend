d5899092147a93d6d7f834e4fd27178770e07556
mixampo <maxim_v_e@hotmail.com>
2019-10-09 12:45:16 +0200

    Empty template first commit

---
+++ src/main/java/rest/ClockManagerRestServer.java
@@ -0,0 +1,13 @@
+package rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("models")
+public class ClockManagerRestServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ClockManagerRestServer.class, args);
+    }
+}
---
+++ src/main/java/rest/security/CustomUserDetailService.java
@@ -0,0 +1,4 @@
+package rest.security;
+
+public class CustomUserDetailService {
+}
