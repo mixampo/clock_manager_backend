33414f8ae196df2b0da1e025da9fbc6306b4a076
mixampo <maxim_v_e@hotmail.com>
2019-10-29 11:43:02 +0100

    Requests workin on normal chrome browser. Added Cors config.

--- src/main/java/rest/ClockManagerRestServer.java
+++ src/main/java/rest/ClockManagerRestServer.java
@@ -3,11 +3,24 @@ package rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("Models")
 public class ClockManagerRestServer {
     public static void main(String[] args) {
         SpringApplication.run(ClockManagerRestServer.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
