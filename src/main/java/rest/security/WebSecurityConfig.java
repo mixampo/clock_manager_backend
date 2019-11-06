1259192913c18f80b07b5f147be5c2d1c2bfdeac
mixampo <maxim_v_e@hotmail.com>
2019-11-06 15:23:04 +0100

    Get worktime registrations working with Jwt

--- src/main/java/rest/security/WebSecurityConfig.java
+++ src/main/java/rest/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package rest.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -39,9 +40,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/signup").permitAll()
                 .antMatchers("/auth").permitAll()
                 .antMatchers("/departments").permitAll()
-                //.antMatchers(HttpMethod.DELETE, "/passwordsets/**").authenticated()
-                //.antMatchers(HttpMethod.GET, "/passwordsets/**").permitAll()
-                //.antMatchers(HttpMethod.POST, "/passwordsets/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/worktime-registrations/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/worktime-registrations/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/worktime-registrations/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
